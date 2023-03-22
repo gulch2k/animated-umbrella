7c03b763cc20451610d531cf19ce2ec6b0f08bbd
jamaal <72471210+gulch2k@users.noreply.github.com>
2023-03-22 07:36:45 -0700

    added files via upload
    block07 workshop

---
+++ block07/script.js
@@ -0,0 +1,23 @@
+/*
+Create a alert box with a message displaying "You have received this message because you have been chosen to open an important vault. 
+Here is the secret combination: 10 - 40 - 39 " 
+
+Add an an additon operator to the first num.
+Add an multiplication operator to the second num.
+Add an division operator to the third num.
+Assign variables to the values of the first num, the second num, and the third num.
+Assign the result to the alert box.
+
+Show the alert box.
+*/
+
+//defining variables
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+const firstNum = 5 + 5;
+const secondNum = 80 / 2;
+const thirdNum = 3 * 13;
+
+//creating alert box
+alert(message + firstNum + " - " + secondNum + " - " + thirdNum);
+
+//console.log(alert)
